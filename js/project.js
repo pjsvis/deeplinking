280cbbd154aca988a4af0d6c2231b753aa8e143a
Peter J Smith <pjstarifa@gmail.com>
2012-04-27 21:14:45 +0100

    Fixed up issue with controller specified but not defined causes  no views to show

---
+++ js/project.js
@@ -0,0 +1,51 @@
+angular.module('project', ['mongolab'])
+    .config(function($routeProvider) {
+        $routeProvider.
+            when('/list', {controller:ListCtrl, template:'../templates/list.html'})
+            .when('/edit/:projectId', {controller:EditCtrl, template:'../templates/detail.html'})
+            .when('/new', {controller:CreateCtrl, template:'../templates/detail.html'})
+            .when('/about', {controller:AboutCtrl, template:'../templates/about.html'})
+            .otherwise({redirectTo:'/list'});
+    });
+
+function AboutCtrl($scope, Project){
+}
+
+function ListCtrl($scope, Project) {
+    $scope.projects = Project.query();
+}
+
+
+function CreateCtrl($scope, $location, Project) {
+    $scope.save = function() {
+        Project.save($scope.project, function(project) {
+            $location.path('/edit/' + project._id.$oid);
+        });
+    }
+}
+
+
+function EditCtrl($scope, $location, $routeParams, Project) {
+    var self = this;
+
+    Project.get({id: $routeParams.projectId}, function(project) {
+        self.original = project;
+        $scope.project = new Project(self.original);
+    });
+
+    $scope.isClean = function() {
+        return angular.equals(self.original, $scope.project);
+    }
+
+    $scope.destroy = function() {
+        self.original.destroy(function() {
+            $location.path('/list');
+        });
+    };
+
+    $scope.save = function() {
+        $scope.project.update(function() {
+            $location.path('/list');
+        });
+    };
+}
